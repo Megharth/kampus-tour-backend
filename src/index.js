c2727f219781ff7f761b62d7e488d388c01a30d1
Megharth <m.c.lakhataria@gmail.com>
2019-08-05 17:14:55 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 dotenv.config()
 
-app.listen(3000, async()=> {
+app.listen(process.env.PORT || 3000, async()=> {
   try {
     console.log('server started')
     const uri = process.env.DB
